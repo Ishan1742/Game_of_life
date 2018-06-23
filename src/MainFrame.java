dd0e97259a93c5ff962db6c7aff89045e907b38a
Ishan Ghosh <ishanghosh1999@gmail.com>
2018-06-23 20:03:30 +0530

    Create JFrame window

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,2 +1,31 @@
-public class MainFrame {
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame
+{
+    private  JPanel mainPanel;
+    public static void main(String[] args)
+    {
+        new MainFrame();
+    }
+
+    public MainFrame()
+    {
+        setTitle("Conway's Game of Life");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setSize(new Dimension(1000, 600));
+
+        setMainPanel();
+
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    private void  setMainPanel()
+    {
+        mainPanel = new JPanel();
+        mainPanel.setBackground(Color.LIGHT_GRAY);
+        this.setContentPane(mainPanel);
+    }
+
 }
